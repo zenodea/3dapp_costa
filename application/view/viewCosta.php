3c015a08d9bdc31265a0d1ba3a2db95a5694b642
“DoveCrying” <zenolino@gmail.com>
2023-05-11 15:22:58 +0100

    Fixed CDN for Poppers.js

--- application/view/viewCosta.php
+++ application/view/viewCosta.php
@@ -861,7 +861,7 @@
         <script src="application/js/jquery.fancybox.min.js" crossorigin="anonymous"></script> 
 
         <!-- Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/@popperjs/core@2"></script>
 
         <!-- X3D -->
         <script src="application/js/x3dom/x3dom.js" crossorigin="anonymous"></script> 
